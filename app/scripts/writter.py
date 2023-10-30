07e493509c18229f2dacdb2b44185b17025fc02c
Ezequiel Gonzalez <quielo@192.168.1.7>
2023-10-29 22:20:58 -0600

    Reading and Writting func of Chunks

---
+++ app/scripts/writter.py
@@ -0,0 +1,25 @@
+from reader import divide_text_into_chunks
+import os
+
+def main():
+    
+    # chunk_list = []
+
+    relative_path = 'Work/projects/python/prod/aibot/app/source_docs'
+    folder_path = os.path.expanduser(f'~/{relative_path}')
+
+    file_list = []
+
+    for file in os.listdir(folder_path):
+        if file.endswith('.txt'):
+            file_path = os.path.join(folder_path, file)
+            file_list.append(file_path)
+            
+    for filename in file_list:
+        chunks = divide_text_into_chunks(filename)
+        for i, chunk in enumerate(chunks):
+            # chunk_list.append(chunk)
+            print(f"Chunk {i + 1}:\n{chunk}\n")
+
+if __name__ == '__main__':
+    main()
