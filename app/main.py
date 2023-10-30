581d3a89584cb561a9a3c00e35615a5c0fe3c249
Ezequiel Gonzalez <quielo@192.168.1.7>
2023-10-30 01:42:59 -0600

    Finished V1.0

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
 import pyautogui as pya
 from scripts.writer import output_chunks
+from scripts.prompts_paths import prompt_chunks
 
-pya.click(1025, 845)
+pya.click(1025, 845)  # first click changes to GPT screen
 pya.sleep(1)
-pya.click(1025, 845)
+pya.click(1025, 845)  # second click activates writting bar
+
+prompt = prompt_chunks()
+
+# pre-typing prompt:
+pya.typewrite(prompt, interval=0.02)
+pya.sleep(1)
+pya.press('enter')
+pya.sleep(2)
 
 chunks = output_chunks()
 
 for chunk in chunks:
-    # chunk = chunk.replace('\n', '\t\n')  # Replace line breaks with "Shift + Enter" (Tab + Enter)
     pya.typewrite(chunk, interval=0.02)
-    pya.sleep(2)
+    pya.sleep(1)
     pya.press('enter')
 
-
-    # pya.click(1402, 844) 
-    # pya.click(1025, 845) # submit button
-
-
-
+# post-typing prompt:
+pya.typewrite("end-of-prompt:  ")
---
+++ app/scripts/prompts_paths.py
@@ -0,0 +1,39 @@
+# import os
+
+# def prompt_chunks():
+
+#     relative_path = 'Work/projects/python/prod/aibot/app/prompts'
+#     folder_path = os.path.expanduser(f'~/{relative_path}')
+#     filename = os.path.join(folder_path)
+
+#     with open(filename, 'r') as file:
+#         file_content = file.read()
+
+#     formatted_text = '   '.join(file_content.splitlines())
+
+#     prompt = formatted_text
+
+#     prompt_output = f"{prompt}"
+#     print(prompt_output)
+
+#     return prompt_output
+
+import os
+
+def prompt_chunks():
+    relative_path = 'Work/projects/python/prod/aibot/app/prompts'
+    folder_path = os.path.expanduser(f'~/{relative_path}')
+    filename = os.path.join(folder_path, 'prompt.txt')  # Replace 'your_input_file.txt' with the actual filename
+
+    try:
+        with open(filename, 'r') as file:
+            file_content = file.read()
+        formatted_text = ' '.join(file_content.splitlines())
+        prompt = formatted_text
+        return prompt
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+        return None
+
+
+
